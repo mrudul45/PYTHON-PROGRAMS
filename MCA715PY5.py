4565239ce8e41ab54b9910277711053244b646b7
mrudul45 <116331519+mrudul45@users.noreply.github.com>
2022-10-21 11:12:35 +0530

    Add files via upload

---
+++ MCA715PY5.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+x=int(input("enter the value of x :"))
+y=int(input("enter the value of y :"))
+print("value of x  and y before swapping :", x , y)
+x = x + y
+y = x - y
+x = x - y 
+
+print("value of x and y after swapping", x ,y)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
