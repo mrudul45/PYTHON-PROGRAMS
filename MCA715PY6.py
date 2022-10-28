223b24291134e6b8e5cfae7e3538f157befc6287
mrudul45 <116331519+mrudul45@users.noreply.github.com>
2022-10-28 11:00:58 +0530

    Add files via upload

---
+++ MCA715PY6.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+
+import cmath as math
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+c=int(input("enter a number"))
+d=(b**2)-(4*a*c)
+sol1=(-b+(math.sqrt(d))/(2*a))
+sol2=(-b-(math.sqrt(d))/(2*a))
+print("square root of equation are  ", sol1,sol2)
+
+
+# In[ ]:
+
+
+
+
