a32972f5dd080a176d7d742dd826817084886946
mrudul45 <116331519+mrudul45@users.noreply.github.com>
2022-11-04 09:48:21 +0530

    Add files via upload

---
+++ MCA715PY9.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+a= int(input("enter first number"))
+b= int(input("enter second number"))
+choice=int(input("enter choice :"))
+if choice==1:
+    sum=a+b
+    print("sum=",sum)
+elif choice==2:
+    diff=a-b
+    print("diff=",diff)
+elif choice==3:
+    div=a/b
+    print("div=",div)
+elif choice==4:
+    mul=a*b
+    print("mul=",mul)
+else:
+    print("invalid option")
+    
+
+
+
+# In[ ]:
+
+
+
+
